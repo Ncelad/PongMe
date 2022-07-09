af94fb0b9f13f8df97b1ba6aea129b7e269779df
Ncelad <ncelad.bat@gmail.com>
2022-07-09 14:15:36 +0300

    Page Switch System. Pages Prototypes.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PongMe.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,13 @@ namespace PongMe
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Dictionary<string,Page> pages { get; private set; } = new Dictionary<string, Page>();
+
         public MainWindow()
         {
             InitializeComponent();
+            pages.Add(typeof(Home).Name,new Home());
+            this.Page.Content = pages["Home"];
             this.Close.Width /= 2;
             this.Close.Height /= 2;
         }
@@ -36,6 +41,47 @@ namespace PongMe
         private void Close_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.Close();
+        }
+
+        //-------------------------------------------------------------------
+
+        private void Logo_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (pages.ContainsKey("Home"))
+            {
+                this.Page.Content = pages["Home"];
+            }
+            else
+            {
+                pages.Add(typeof(Home).Name, new Home());
+                this.Page.Content = pages["Home"];
+            }
+        }
+
+        private void Avatar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (pages.ContainsKey("Example"))
+            {
+                this.Page.Content = pages["Example"];
+            }
+            else
+            {
+                pages.Add(typeof(Example).Name, new Example());
+                this.Page.Content = pages["Example"];
+            }
+        }
+
+        private void Settings_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (pages.ContainsKey("Settings"))
+            {
+                this.Page.Content = pages["Settings"];
+            }
+            else
+            {
+                pages.Add(typeof(Settings).Name, new Settings());
+                this.Page.Content = pages["Settings"];
+            }
         }
     }
 }
